63f57f76730a5d5a68a06988234af2aea0308fdd
Jason Gionfriddo <Ragtop66goat@gmail.com>
2024-03-12 20:10:54 -0500

    added Review.java in the entities directory, added ReviewRepository.java to dao directory, and updated MyDataRestConfig.java to expose the Reviews id's

--- src/main/java/com/ragtopmedia/libraryappbackend/config/MyDataRestConfig.java
+++ src/main/java/com/ragtopmedia/libraryappbackend/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.ragtopmedia.libraryappbackend.config;
 
 import com.ragtopmedia.libraryappbackend.entities.Book;
+import com.ragtopmedia.libraryappbackend.entities.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -23,8 +24,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 HttpMethod.PUT};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
